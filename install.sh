277045cde4f750e2d8b2fb8ab36ff48cfd5e52bf
Edouard Dupin <yui.heero@gmail.com>
2012-05-10 12:00:22 +0200

    Corection of the basic install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 #really simple to install
 sudo cp linux/bin/release/edn /usr/bin/edn
 sudo chmod 777 /usr/bin/edn
 sudo mkdir -p /usr/share/edn
 sudo rm -r /usr/share/edn/*
-sudo cp assets/* /usr/share/edn/
+sudo cp -r assets/* /usr/share/edn/
