a210af5627bbd7a37d69ade675d8e565ba4c4550
Edouard Dupin <yui.heero@gmail.com>
2012-05-22 09:49:11 +0200

    [BREAK compatibility] Update to the new input call function

--- jni/appl/Gui/CodeView.cpp
+++ jni/appl/Gui/CodeView.cpp
@@ -231,21 +231,22 @@ bool CodeView::OnEventKbMove(ewol::eventKbType_te typeEvent, ewol::eventKbMoveTy
 
 /**
  * @brief Event on an input of this Widget
+ * @param[in] type Type of the input (ewol::INPUT_TYPE_MOUSE/ewol::INPUT_TYPE_FINGER ...)
  * @param[in] IdInput Id of the current Input (PC : left=1, right=2, middle=3, none=0 / Tactil : first finger=1 , second=2 (only on this widget, no knowledge at ouside finger))
  * @param[in] typeEvent ewol type of event like EVENT_INPUT_TYPE_DOWN/EVENT_INPUT_TYPE_MOVE/EVENT_INPUT_TYPE_UP/EVENT_INPUT_TYPE_SINGLE/EVENT_INPUT_TYPE_DOUBLE/...
  * @param[in] pos Absolute position of the event
  * @return true the event is used
  * @return false the event is not used
  */
-bool CodeView::OnEventInput(int32_t IdInput, ewol::eventInputType_te typeEvent, coord2D_ts pos)
+bool CodeView::OnEventInput(ewol::inputType_te type, int32_t IdInput, ewol::eventInputType_te typeEvent, coord2D_ts pos)
 {
 	coord2D_ts relativePos = RelativePosition(pos);
 	if (m_bufferID < 0) {
 		return false;
 	}
-	if (true == WidgetScrooled::OnEventInput(IdInput, typeEvent, pos)) {
+	if (true == WidgetScrooled::OnEventInput(type, IdInput, typeEvent, pos)) {
 		ewol::widgetManager::FocusKeep(this);
-		// nothing to do ... done on upper widet ...
+		// nothing to do ... done on upper widget ...
 		return true;
 	}
 	if (1 == IdInput) {
