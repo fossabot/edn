aea60950ff0a642ec1585f48e9edc18eb9c6a858
Edouard Dupin <yui.heero@gmail.com>
2012-05-29 12:57:07 +0200

    Change the image needed in edn

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 #really simple to install
-sudo cp linux/bin/release/edn /usr/bin/edn
+echo "- copy binary /usr/bin/edn"
+sudo cp -vf linux/bin/release/edn /usr/bin/edn
 sudo chmod 777 /usr/bin/edn
 sudo mkdir -p /usr/share/edn
-sudo rm -r /usr/share/edn/*
-sudo cp -r assets/* /usr/share/edn/
+echo "- Remove the folder /usr/share/edn/*"
+sudo rm -rf /usr/share/edn/*
+sudo cp -rfv assets/* /usr/share/edn/
