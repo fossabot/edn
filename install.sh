d3e963749ba9eb915809a64193f54f8a5fbf410e
Edouard Dupin <yui.heero@gmail.com>
2012-08-15 11:48:23 +0200

    change install on PC basic folder

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 #really simple to install
 echo "- copy binary /usr/bin/edn"
-sudo cp -vf out_LINUX/release/obj/edn/edn /usr/bin/edn
+sudo cp -vf out_Linux/release/obj/edn/edn /usr/bin/edn
 sudo chmod 777 /usr/bin/edn
 sudo mkdir -p /usr/share/edn
 echo "- Remove the folder /usr/share/edn/*"
