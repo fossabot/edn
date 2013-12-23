2fc47723f0d84ca8529cc2281d8fa236d5399a36
Edouard DUPIN <yui.heero@gmail.com>
2013-12-23 22:52:33 +0100

    [DEV] add monk doc generator

---
+++ sources/monk_edn.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import monkModule as module
+import monkTools as tools
+import monkDebug as debug
+import datetime
+
+
+def get_desc():
+	return "EDN application N'ours editor (Editeur De N'ours)"
+
+def create():
+	# module name is 'edn' and type binary.
+	myModule = module.Module(__file__, 'edn', 'APPLICATION')
+	# enable doculentation :
+	myModule.set_website("http://heeroyui.github.io/edn/")
+	myModule.set_path(tools.get_current_path(__file__) + "/appl/")
+	
+	# add the currrent module at the 
+	return myModule
+
+
