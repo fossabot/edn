f9ff0b8899ece29e55ec497a2b36e8cdad8468fd
Edouard DUPIN <yui.heero@gmail.com>
2014-09-18 21:07:32 +0200

    [DEBUG] remove lutinBase.py that is deprecated

--- lutinBase.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import lutinModule
-import lutinTools
-
-lutinModule.import_path(lutinTools.get_current_path(__file__) + "/sources/")
-lutinModule.import_path(lutinTools.get_current_path(__file__) + "/../ewol/")
